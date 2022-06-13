4e7b207f004558cafb546efd97c17763c9583e44
kedrakk <keaindrak@gmail.com>
2022-06-13 20:51:51 +0630

    add config

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,6 +28,8 @@ class AuthServiceProvider extends ServiceProvider
 
         if (!$this->app->routesAreCached()) {
             Passport::routes();
+            Passport::tokensExpireIn(now()->addDays(15));
+            Passport::refreshTokensExpireIn(now()->addDays(30));
         }
     }
 }
